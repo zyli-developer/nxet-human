e2a2223b637d8f82b2ff8377fd9bc21f6c9cf4a3
zyli <lizhenyu0613@126.com>
2026-01-05 15:28:35 +0800

    refactor: comment out authentication logic in ProtectedRoute component

--- src/RouteGuard/index.js
+++ src/RouteGuard/index.js
@@ -2,12 +2,12 @@ import { Navigate, useLocation } from 'react-router-dom';
 
 
 const ProtectedRoute = ({ children }) => {
-  const location = useLocation();
-  const isAuthenticated = sessionStorage.getItem('isAuthenticated') === 'true';
+  // const location = useLocation();
+  // const isAuthenticated = sessionStorage.getItem('isAuthenticated') === 'true';
 
-  if (!isAuthenticated) {
-    return <Navigate to="/" state={{ from: location }} replace />;
-  }
+  // if (!isAuthenticated) {
+  //   return <Navigate to="/" state={{ from: location }} replace />;
+  // }
 
   return <>{children}</>;
 }
